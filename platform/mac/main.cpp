730db87348035c822f0cd9cd719b0519a749220d
qiruisun_mac <sqr@detu.com>
2019-06-18 23:43:35 +0800

    add

--- platform/mac/main.cpp
+++ platform/mac/main.cpp
@@ -21,22 +21,27 @@ int main(int argc, char** argv)
     int type = 0;
     if (argc >= 2)
     {
-        type = std::stoul(std::string(argv[1]));
+        type = std::atoi(argv[1]);
     }
-    std::string kernel_file ;
-    fly::ComputeAPI kernel_type;
-    std::string kernel_function;
+    std::string kernel_file = "";
+    fly::ComputeAPI kernel_type = fly::ComputeAPI::OpenCL;
+    std::string kernel_function = "";
     switch (type) {
       
         case 0:
-            kernel_file = "/Users/qiruisun/github/fly_gpgpu2/fly_demo/demo1/opencl_kernel.cl";
+            kernel_file = "/Users/qiruisun/github/fly_gpgpu/fly_demo/demo1/opencl_kernel.cl";
             kernel_type = fly::ComputeAPI::OpenCL;
             kernel_function = "vectorAddition";
             break;
         case 1:
-            kernel_file = "/Users/qiruisun/github/fly_gpgpu2/fly_demo/demo1/vulkan_kernel.glsl";
+            kernel_file = "/Users/qiruisun/github/fly_gpgpu/fly_demo/demo1/vulkan_kernel.glsl";
             kernel_type = fly::ComputeAPI::Vulkan;
             kernel_function = "main";
+            break;
+        case 2:
+            kernel_file = "/Users/qiruisun/github/fly_gpgpu/fly_demo/demo1/cuda_kernel.cu";
+            kernel_type = fly::ComputeAPI::CUDA;
+            kernel_function = "vectorAddition";
             break;
             
         default:
